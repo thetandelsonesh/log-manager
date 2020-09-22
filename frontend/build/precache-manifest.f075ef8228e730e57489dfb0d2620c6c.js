f83f4cd611b428ab49887a8928db2c56c480039c
Sonesh Tandel <thetandelsonesh@gmail.com>
2020-09-22 19:58:53 +0530

    build

---
+++ frontend/build/precache-manifest.f075ef8228e730e57489dfb0d2620c6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df37c59ac7aaf7eee072cbfa80766d46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96a8dd945045bccd0001",
+    "url": "/static/css/2.7d30f6f2.chunk.css"
+  },
+  {
+    "revision": "79115b95748d280626bf",
+    "url": "/static/css/main.8b07a4cd.chunk.css"
+  },
+  {
+    "revision": "96a8dd945045bccd0001",
+    "url": "/static/js/2.71508c82.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.71508c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79115b95748d280626bf",
+    "url": "/static/js/main.c0873a4c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "125952b3034d74f6f6d397265c32b971",
+    "url": "/static/media/logo.125952b3.png"
+  }
+]);
